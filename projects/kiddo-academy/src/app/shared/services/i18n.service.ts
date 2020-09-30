8ecf3c62430300844683351c0491f5d8c32a5b12
Alexander Dedenev <alexander.dedenev@dataart.com>
2020-09-30 10:01:02 +0300

    Implement lang config by url GET parameter

--- projects/kiddo-academy/src/app/shared/services/i18n.service.ts
+++ projects/kiddo-academy/src/app/shared/services/i18n.service.ts
@@ -2,6 +2,7 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { environment } from 'projects/kiddo-academy/src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,32 +11,37 @@ export class I18nService {
 
   constructor(
     private translateService: TranslateService,
-    private http: HttpClient,
+    private http: HttpClient
   ) { }
 
   private readonly timestamp = Date.now();
 
-  processLanguageConfiguration(): Promise<void> {
-    return this.http.get<string[]>(`/assets/i18n/availableLangs.json?timestamp=${this.getTimestamp()}`)
-      .toPromise()
-      .then(availableLanguages => {
-        this.translateService.addLangs(availableLanguages);
+  async processLanguageConfiguration(): Promise<void> {
+    const availableLanguages = await this.http.get<string[]>(`/assets/i18n/availableLangs.json?timestamp=${this.getTimestamp()}`)
+    .toPromise();
 
-        const savedLang = localStorage.getItem(environment.localStorageKeys.selectedLanguage);
-        const browserLang = this.translateService.getBrowserLang();
+    const url = new URL(window.location.href);
+    const paramsLang = url.searchParams.get('lang');
 
-        if (savedLang && this.translateService.getLangs().includes(savedLang)) {
-          this.translateService.use(savedLang);
-        } else if (this.translateService.getLangs().includes(browserLang)) {
-          this.translateService.setDefaultLang(browserLang);
-          this.translateService.use(browserLang);
-        } else {
-          this.translateService.setDefaultLang('ru');
-          this.translateService.use('ru');
-        }
+    this.translateService.addLangs(availableLanguages);
 
-        return;
-      });
+    const savedLang = localStorage.getItem(environment.localStorageKeys.selectedLanguage);
+    const browserLang = this.translateService.getBrowserLang();
+
+    if (paramsLang && this.translateService.getLangs().includes(paramsLang)) {
+      localStorage.setItem(environment.localStorageKeys.selectedLanguage, paramsLang);
+      this.translateService.use(paramsLang);
+    } else if (savedLang && this.translateService.getLangs().includes(savedLang)) {
+      this.translateService.use(savedLang);
+    } else if (this.translateService.getLangs().includes(browserLang)) {
+      this.translateService.setDefaultLang(browserLang);
+      this.translateService.use(browserLang);
+    } else {
+      this.translateService.setDefaultLang('ru');
+      this.translateService.use('ru');
+    }
+
+
   }
 
   changeCurrentLanguage(lang: string): void {
