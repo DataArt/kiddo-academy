a6ca55b0ba7f85fcd55164caa7429c6d9b2b7b61
Nabi Isakhanov <nabi.isakhanov@dataart.com>
2021-01-31 23:46:38 +0300

    Fix: remove console log

--- projects/kiddo-academy/src/app/shared/services/new-year.service.ts
+++ projects/kiddo-academy/src/app/shared/services/new-year.service.ts
@@ -11,7 +11,6 @@ export class NewYearService {
     const now = new Date();
     const start = new Date(now.getFullYear(), 11, 20);
     const end = new Date(now.getFullYear(), 1, 1);
-    console.log({now, start, end});
 
     return now.getTime() > start.getTime() || now.getTime() < end.getTime();
   }
